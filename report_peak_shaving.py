7432793fd06c38cd445ff1d430a7a5e9b247bda1
SAVSoleol <formationsoleol@gmail.com>
2026-09-09 11:21:32 +0200

    Update report_peak_shaving.py

--- report_peak_shaving.py
+++ report_peak_shaving.py
@@ -112,7 +112,7 @@ def _side_bar(
 
     logo = _resolve_logo_path(logo_path)
     if logo:
-        pdf.image(logo, x=6, y=30, w=40)
+        pdf.image(logo, x=5, y=25, w=40)
     else:
         pdf.set_xy(8, 12)
         pdf.set_font("Arial", "B", 16)
